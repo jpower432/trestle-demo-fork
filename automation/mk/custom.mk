6d83e69dff28df733c313eec2cfb4f2aa0585877
Jennifer Power <barnabei.jennifer@gmail.com>
2023-05-02 17:58:55 -0400

    feat: moves automation to directory managed by git subtree
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- automation/mk/custom.mk
+++
@@ -1,7 +0,0 @@
-############################################################################
-## Custom Workflow
-############################################################################
-
-generate-custom-ssp:
-	@source ./automation/trestle.sh && trestle author ssp-generate -p ACME_internal_profile --compdefs hello-world-custom -o markdown/system-security-plans/acme_custom_demo_ssp -f
-.PHONY: generate-custom-ssp
